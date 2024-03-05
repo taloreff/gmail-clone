2dd5f47b281371c4dbaa97da3837870b13e5ac7e
talor <taloreff@gmail.com>
2024-03-05 15:40:21 +0200

    github pages installed

--- src/cmps/EmailFilter.jsx
+++ src/cmps/EmailFilter.jsx
@@ -1,32 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 export function EmailFilter({ filterBy, onSetFilter }) {
-    const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
+    const [filterByToEdit, setFilterByToEdit] = useState(filterBy);
 
     useEffect(() => {
-        onSetFilter(filterByToEdit)
-    }, [filterByToEdit])
+        onSetFilter(filterByToEdit);
+    }, [filterByToEdit]);
 
     function onSubmitFilter(ev) {
-        ev.preventDefault()
-        onSetFilter(filterByToEdit)
+        ev.preventDefault();
+        onSetFilter(filterByToEdit);
     }
 
     function handleChange(ev) {
-        let { value } = ev.target
-        setFilterByToEdit(prevFilter => ({ ...prevFilter, subject: value, body: value, from: value, to: value }))
+        let { value } = ev.target;
+        setFilterByToEdit(prevFilter => ({ ...prevFilter, subject: value, body: value, from: value, to: value }));
     }
 
-    return <form className="email-filter" onSubmit={onSubmitFilter} >
-        <input
-            type="text"
-            placeholder="Search mail"
-            value={filterByToEdit.type}
-            name="mail"
-            onChange={handleChange}
-        />
-    </form>
+    return (
+        <form className="email-filter" onSubmit={onSubmitFilter}>
+            <div className="email-filter-input">
+                <FontAwesomeIcon icon={faMagnifyingGlass} className="email-filter-icon" />
+                <input
+                    type="text"
+                    value={filterByToEdit.type}
+                    name="mail"
+                    onChange={handleChange}
+                    placeholder="Search mail"
+                />
+            </div>
+        </form>
+    );
 }
 
 EmailFilter.propTypes = {
